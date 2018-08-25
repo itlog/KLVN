7b80d55809a5d57289a528c1059729c6fc31b94e
kc13666 <kc13666@192.168.1.121>
2018-08-25 14:14:47 +0800

    v0.01

---
+++ KLVNBatchWeb/src/ml/kelvinco/java/util/KLVNJConstants.java
@@ -0,0 +1,11 @@
+package ml.kelvinco.java.util;
+
+public class KLVNJConstants {
+	
+	protected final static String APP_NAME    = "KLVNBatch";
+	protected final static String APP_VERSION = "0.01";
+	
+	
+	
+
+}
---
+++ KLVNBatchWeb/src/ml/kelvinco/java/util/KLVNJLogger.java
@@ -0,0 +1,5 @@
+package ml.kelvinco.java.util;
+
+public class KLVNJLogger {
+
+}
